ebabc87bbf9c41b9818feb45196e6183fe942be4
Denis <denis.s@maxitechs.com>
2019-11-26 18:01:25 +0200

    add codecov

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# browser-event-registry
+# browser-event-register
 [![Build Status](https://travis-ci.com/DisaPadla/browser-event-register.svg?branch=master)](https://travis-ci.com/DisaPadla/browser-event-register)
--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
-  coverageReporters: [
-    "json-summary"
-  ]
+  "coverageDirectory": "./coverage/",
+  "collectCoverage": true
 };
